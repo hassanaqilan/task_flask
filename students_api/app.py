a8497176a5cecc20b7d5d64846dbcc03cadd5298
hassanaqilan <hassan.aqilan@sociumtech.com>
2025-03-02 14:50:52 +0300

    -

--- students_api/app.py
+++ students_api/app.py
@@ -6,7 +6,8 @@ def create_app() -> Flask:
     app = Flask(__name__)
     student_view = StudentAPI().instance()
 
-    app.add_url_rule('/students/', view_func=student_view, methods=['POST', 'GET'])
+    app.add_url_rule('/students/', view_func=student_view,
+                     methods=['POST', 'GET'])
     app.add_url_rule(
         '/students/<int:student_id>',
         view_func=student_view,
